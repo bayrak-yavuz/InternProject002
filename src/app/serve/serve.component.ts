94401d7ef3479708798c554b99ac2a27f54bb2a6
Mustafa Alperen Karakuş <alperenkarakus062002@gmail.com>
2021-10-21 18:31:01 +0300

    text ts aktarıldı alp

--- src/app/serve/serve.component.ts
+++ src/app/serve/serve.component.ts
@@ -10,6 +10,12 @@ import { Component, OnInit } from '@angular/core';
 export class ServeComponent implements OnInit {
   serveTitle1="Hizmetlerimiz"
   serveText1="Netger'in uzman ekibiyle işinizi daha ileriye taşıyacağımız hizmetlerimiz!"
+  buton1Text="Dijital Pazarlama"
+  buton2Text="Web Tasarım"
+  buton3Text="Sosyal Medya Danışmanlığı"
+  buton4Text="Mobil Uygulama Geliştirme"
+  buton5Text="Fotograf ve Video Çekimi"
+
   constructor() { }
 
   ngOnInit(): void {
