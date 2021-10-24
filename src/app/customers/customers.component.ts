cc8fcb81c02a73ff8799c10d7bdcc749546d9757
Mustafa Alperen Karakuş <alperenkarakus062002@gmail.com>
2021-10-24 18:46:26 +0300

    customers ve serve html kısaltıldı

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -12,6 +12,171 @@ export class CustomersComponent implements OnInit {
   textClient="Dijital hizmetlerde tercihini Netger'den yana kullanan bazı müşterilerimiz!"
   constructor() { }
 
+
+  images=[
+    {
+    src:"../../assets/img-customers/ref-1.jpg",
+  
+  },
+    {
+
+    
+      src:"../../assets/img-customers/ref-2jpg.jpg",}
+      ,
+    {
+
+    
+      src:"../../assets/img-customers/ref-3.jpg",
+    },
+ 
+      {
+
+    
+      src:"../../assets/img-customers/ref-4.jpg",
+    },
+ 
+      {
+
+    
+      src:"../../assets/img-customers/ref-5.jpg",
+    },
+ 
+      {
+
+    
+      src:"../../assets/img-customers/ref-6.jpg",
+    },
+ 
+      {
+
+    
+      src:"../../assets/img-customers/ref-7.jpg",
+    },
+ 
+      {
+
+    
+      src:"../../assets/img-customers/ref-8.jpg",
+    },
+ 
+      {
+
+    
+      src:"../../assets/img-customers/ref-9.jpg",
+    },
+ 
+      {
+
+    
+      src:"../../assets/img-customers/ref-10.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-11.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-12.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-13.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-14.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-15.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-16.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-17.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-18.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-19.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-20.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-21.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-22.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-23.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-24.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-25.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-26.jpg",
+    },
+
+      {
+
+    
+      src:"../../assets/img-customers/ref-27.jpg",
+    },
+
+      
+
+  ]
+
   ngOnInit(): void {
   }
 
