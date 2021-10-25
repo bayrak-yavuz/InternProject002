632c1f39ac566eede930b089c3511179e1c13261
durdane ayşe <78816462+DurdaneAyse@users.noreply.github.com>
2021-10-25 15:34:16 +0300

    ts

--- src/app/referances/referances.component.ts
+++ src/app/referances/referances.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./referances.component.scss']
 })
 export class ReferancesComponent implements OnInit {
-
+   icon=false;
+  images=[
+    { 
+    src: "/assets/uyafa.webp",
+    text: "Uyafa",
+    url: "https://www.uyafa.com/",        
+  },
+  {
+    src: "/assets/akselimoglu.webp",
+    text: " Akselimoğlu Gıda",
+    url: "https://www.akselimoglu.com/",
+    
+  },
+  {
+    src: "/assets/tuze.webp",
+    text: "Tüze Hukuk Bürosu",
+    url: "https://tuzeavukatlik.com/",
+    
+  },
+  {
+    src: "/assets/erkamkomurcu.webp",
+    text: "Dç.Dr. Erkam KÖMÜRCÜ",
+    url: "https://www.erkamkomurcu.com/",
+    
+  },
+]
   constructor() { }
 
   ngOnInit(): void {
