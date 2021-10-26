7f336eaab8e44cda709741f0db7de873c675d014
durdane ayşe <78816462+DurdaneAyse@users.noreply.github.com>
2021-10-26 18:20:52 +0300

    olmayan

--- src/app/campaign/campaign.component.ts
+++ src/app/campaign/campaign.component.ts
@@ -7,6 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampaignComponent implements OnInit {
 
+  camp = [
+    {
+      src: "/assets/kampanya-3.jpg",
+
+    },
+    {
+      src: "/assets/kampanya.jpg",
+
+    },
+
+    {
+      src: "/assets/kampanya-2.jpg",
+    },
+  ]
+  camp2 = [
+    {
+      
+      head: "Kurumsal Web Tasarım",
+      text: "Firmanıza özel hazırlanmış bir internet siteniz olsun!",
+    },
+    //  {
+    //     head: "Netger Avukat ürününde %20 indirim Fırsatı",
+    //    text: "Ofisini son 1 yıl içinde açan avukatlara özel!",
+    //   },
+    //  {
+    //     head: "Netger Doktor ürününde %20 İndirim Fırsatı",
+    //     text: "Özel muayenehanesini son 1 yıl içinde açan doktorlara özel!",
+    //   }
+  ]
+  camp3=[
+    { 
+      text:" daha fazla bilgi ",
+      icon:"fas fa-arrow-right",
+      url:"https://netger.net/"
+     
+    }
+  ]
+  camp4 = [
+    {
+      src: "/assets/kampanya-3-m.jpg",
+
+    },
+    {
+      src: "/assets/kampanya-m.jpg",
+
+    },
+
+    {
+      src: "/assets/kampanya-2-m.jpg",
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
